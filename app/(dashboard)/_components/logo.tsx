ff780fc2ba8698a5bd2fd6635967f90ec3decb2c
Subhadeep Adhikari <subhadeepadhikari5@gmail.com>
2023-12-19 15:11:15 +0530

    Initial commit

---
+++ app/(dashboard)/_components/logo.tsx
@@ -0,0 +1,13 @@
+import Image from "next/image";
+
+export const Logo = () => {
+  return (
+    <a href="https://bytelearn-io.vercel.app/" className="text-3xl font-bold" style={
+      {
+        color: "#050921"
+      }
+    }>
+      ByteLearn
+    </a>
+  );
+};
