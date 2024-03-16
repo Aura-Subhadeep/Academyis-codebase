17b991cb5a4b37acadee09faef981b30d43693c3
Subhadeep Adhikari <subhadeepadhikari5@gmail.com>
2024-03-16 15:37:19 +0530

    Update icon-badge.tsx

--- components/icon-badge.tsx
+++ components/icon-badge.tsx
@@ -8,8 +8,8 @@ const backgroundVariants = cva(
   {
     variants: {
       variant: {
-        default: "bg-sky-100",
-        success: "bg-emerald-100",
+        default: "icon-bg",
+        success: "icon-bg",
       },
       size: {
         default: "p-2",
@@ -28,8 +28,8 @@ const iconVariants = cva(
   {
     variants: {
       variant: {
-        default: "text-sky-700",
-        success: "text-emerald-700",
+        default: "icon-bg",
+        success: "icon-bg",
       },
       size: {
         default: "h-8 w-8",
