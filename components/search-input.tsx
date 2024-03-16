bf1e392fdf8c603f20663299991382e6480db4a5
Subhadeep Adhikari <subhadeepadhikari5@gmail.com>
2024-03-16 21:38:19 +0530

    Update search-input.tsx

--- components/search-input.tsx
+++ components/search-input.tsx
@@ -38,7 +38,7 @@ export const SearchInput = () => {
       <Input
         onChange={(e) => setValue(e.target.value)}
         value={value}
-        className="w-full md:w-[300px] pl-9 rounded-full bg-slate-100 focus-visible:ring-slate-200"
+        className="w-full md:w-[400px] pl-28 rounded-md bg-gray-50 focus-visible:ring-slate-200"
         placeholder="Search for a course"
       />
     </div>
