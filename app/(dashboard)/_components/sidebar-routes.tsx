257e9da59db72f7712c423427111c6abb21f7e2f
Subhadeep Adhikari <subhadeepadhikari5@gmail.com>
2024-03-16 12:39:37 +0530

    new icos

--- app/(dashboard)/_components/sidebar-routes.tsx
+++ app/(dashboard)/_components/sidebar-routes.tsx
@@ -1,21 +1,36 @@
 "use client";
 
-import { BarChart, Compass, Layout, List } from "lucide-react";
+import { BarChart, Home, List, Airplay, MessageCircle, Settings, PieChart} from "lucide-react";
 import { usePathname } from "next/navigation";
 
 import { SidebarItem } from "./sidebar-item";
 
 const guestRoutes = [
   {
-    icon: Layout,
+    icon: Home,
     label: "Dashboard",
     href: "/",
   },
   {
-    icon: Compass,
-    label: "Browse",
+    icon: Airplay,
+    label: "Courses",
     href: "/search",
   },
+  {
+    icon: MessageCircle,
+    label: "Chat",
+    href: "/profile",
+  },
+  {
+    icon: PieChart,
+    label: "Grades",
+    href: "/analytics",
+  },
+  {
+    icon: Settings,
+    label: "Settings",
+    href: "/courses",
+  }
 ];
 
 const teacherRoutes = [
