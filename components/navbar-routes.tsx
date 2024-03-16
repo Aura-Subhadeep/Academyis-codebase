d66afb9a6d7972973a2b9703b4e90c1e446b3e8e
Subhadeep Adhikari <subhadeepadhikari5@gmail.com>
2024-03-16 21:38:33 +0530

    navbar headings

--- components/navbar-routes.tsx
+++ components/navbar-routes.tsx
@@ -22,25 +22,31 @@ export const NavbarRoutes = () => {
 
   return (
     <>
+    {isSearchPage && ( // Show heading only on the dashboard page
+        <h1 className="hidden md:block font-bold text-2lx px-2 pr-8">Courses</h1>
+      )}
       {isSearchPage && (
         <div className="hidden md:block">
           <SearchInput />
         </div>
       )}
       {isDashboardPage && ( // Show heading only on the dashboard page
-        <h1 className="hidden md:block font-bold text-2xl px-2">Dashboard</h1>
+        <h1 className="hidden md:block font-bold text-2lx px-2">Dashboard</h1>
+      )}
+      {isTeacherPage && ( // Show heading only on the dashboard page
+        <h1 className="hidden md:block font-bold text-2lx px-2">My Courses</h1>
       )}
       <div className="flex gap-x-4 mr-2.5 ml-auto">
         {isTeacherPage || isCoursePage ? (
           <Link href="/">
-            <Button size="sm" variant="ghost">
+            <Button className="border bg-black hover:bg-gray-800 text-white hover:text-gray-200" size="sm" variant="ghost">
               <LogOut className="h-4 w-4 mr-2" />
               Exit
             </Button>
           </Link>
         ) : isTeacher(userId) ? (
           <Link href="/teacher/courses">
-            <Button className="border" size="sm" variant="ghost">
+            <Button className="border bg-black hover:bg-gray-800 text-white hover:text-gray-200" size="sm" variant="ghost">
               Advance
             </Button>
           </Link>
