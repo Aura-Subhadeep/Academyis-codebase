1785ee517f3ed382677ff4de281a5ab612d677c1
Subhadeep Adhikari <subhadeepadhikari5@gmail.com>
2024-03-16 10:54:43 +0530

    rebased code

---
+++ scripts/seed.ts
@@ -0,0 +1,25 @@
+const { PrismaClient } = require("@prisma/client");
+
+const database = new PrismaClient();
+
+async function main() {
+  try {
+    await database.category.createMany({
+      data: [
+        { name: "Computer Engineering" },
+        { name: "Humanities" },
+        { name: "Commerce" },
+        { name: "Data Science" },
+        { name: "Creativity" },
+      ]
+    });
+
+    console.log("Success");
+  } catch (error) {
+    console.log("Error seeding the database categories", error);
+  } finally {
+    await database.$disconnect();
+  }
+}
+
+main();
