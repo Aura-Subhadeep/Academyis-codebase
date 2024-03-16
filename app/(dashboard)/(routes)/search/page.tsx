fbeda6f00a14d19bb32719c2bfbc0db92780b9d8
Subhadeep Adhikari <subhadeepadhikari5@gmail.com>
2024-03-16 21:38:03 +0530

    remove category

--- app/(dashboard)/(routes)/search/page.tsx
+++ app/(dashboard)/(routes)/search/page.tsx
@@ -40,14 +40,8 @@ const SearchPage = async ({
       <div className="px-6 pt-6 md:hidden md:mb-0 block">
         <SearchInput />
       </div>
-      <div className="p-6 space-y-4">
-        <Categories
-          items={categories}
-        />
-        <CoursesList items={courses} />
-      </div>
     </>
-   );
+  );
 }
- 
+
 export default SearchPage;
