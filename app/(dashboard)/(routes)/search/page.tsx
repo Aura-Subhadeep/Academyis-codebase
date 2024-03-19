ed5beafd0a5a5e4c173e4079561b099af0f4b8d1
Subhadeep Adhikari <subhadeepadhikari5@gmail.com>
2024-03-19 13:24:52 +0530

    Update page.tsx

--- app/(dashboard)/(routes)/search/page.tsx
+++ app/(dashboard)/(routes)/search/page.tsx
@@ -40,6 +40,9 @@ const SearchPage = async ({
       <div className="px-6 pt-6 md:hidden md:mb-0 block">
         <SearchInput />
       </div>
+      <div className="p-6 space-y-4">
+        <CoursesList items={courses} />
+      </div>
     </>
   );
 }
