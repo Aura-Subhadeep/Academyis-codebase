7e06537b4da212b84096f8bb652ac9c5fb4a7913
Subhadeep Adhikari <subhadeepadhikari5@gmail.com>
2024-03-19 13:24:46 +0530

    Update course-card.tsx

--- components/course-card.tsx
+++ components/course-card.tsx
@@ -1,6 +1,6 @@
 import Image from "next/image";
 import Link from "next/link";
-import { BookOpen } from "lucide-react";
+import { BookOpen, LayoutTemplate } from "lucide-react";
 
 import { IconBadge } from "@/components/icon-badge";
 import { formatPrice } from "@/lib/format";
@@ -27,7 +27,7 @@ export const CourseCard = ({
 }: CourseCardProps) => {
   return (
     <Link href={`/courses/${id}`}>
-      <div className="group hover:shadow-sm transition overflow-hidden border rounded-lg p-3 h-full">
+      <div className="group hover:shadow-sm transition overflow-hidden border rounded-custom p-3 h-full">
         <div className="relative w-full aspect-video rounded-md overflow-hidden">
           <Image
             fill
@@ -36,8 +36,8 @@ export const CourseCard = ({
             src={imageUrl}
           />
         </div>
-        <div className="flex flex-col pt-2">
-          <div className="text-lg md:text-base font-medium group-hover:text-sky-700 transition line-clamp-2">
+        <div className="flex flex-col pt-2 px-1.5">
+          <div className="text-lg md:text-base font-medium group-hover:text-gray-600 transition line-clamp-2">
             {title}
           </div>
           <p className="text-xs text-muted-foreground">
@@ -45,7 +45,7 @@ export const CourseCard = ({
           </p>
           <div className="my-3 flex items-center gap-x-2 text-sm md:text-xs">
             <div className="flex items-center gap-x-1 text-slate-500">
-              <IconBadge size="sm" icon={BookOpen} />
+              <IconBadge size="sm" icon={LayoutTemplate} />
               <span>
                 {chaptersLength} {chaptersLength === 1 ? "Chapter" : "Chapters"}
               </span>
@@ -58,7 +58,7 @@ export const CourseCard = ({
               value={progress}
             />
           ) : (
-            <p className="text-md md:text-sm font-medium text-slate-700">
+            <p className="text-md md:text-md font-medium text-slate-700">
               {formatPrice(price)}
             </p>
           )}
