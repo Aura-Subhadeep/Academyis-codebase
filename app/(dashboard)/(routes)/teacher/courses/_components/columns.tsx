4e50c1623ac15128c941797c829aeca4f50ff242
Subhadeep Adhikari <subhadeepadhikari5@gmail.com>
2024-03-21 20:41:08 +0530

    Update columns.tsx

--- app/(dashboard)/(routes)/teacher/courses/_components/columns.tsx
+++ app/(dashboard)/(routes)/teacher/courses/_components/columns.tsx
@@ -47,7 +47,8 @@ export const columns: ColumnDef<Course>[] = [
       const price = parseFloat(row.getValue("price") || "0");
       const formatted = new Intl.NumberFormat("en-US", {
         style: "currency",
-        currency: "INR"
+        currency: "INR",
+        minimumFractionDigits: 0,
       }).format(price);
 
       return <div>{formatted}</div>
