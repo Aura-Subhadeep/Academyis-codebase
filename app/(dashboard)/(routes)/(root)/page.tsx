e825a2be61b8e3996c8c23c1c12431b10351db93
Subhadeep Adhikari <subhadeepadhikari5@gmail.com>
2024-03-23 10:09:50 +0530

    reverted dashboard stats

--- app/(dashboard)/(routes)/(root)/page.tsx
+++ app/(dashboard)/(routes)/(root)/page.tsx
@@ -7,7 +7,6 @@ import { CoursesList } from "@/components/courses-list";
 
 import { InfoCard } from "./_components/info-card";
 import { IconBadge } from "@/components/icon-badge";
-import Image from "next/image";
 
 export default async function Dashboard() {
   const { userId } = auth();
@@ -22,45 +21,44 @@ export default async function Dashboard() {
   } = await getDashboardCourses(userId);
 
   return (
-      <>
         <div className="p-6 space-y-4">
-          <div className="grid grid-cols-2 sm:grid-cols-4 gap-4">
-            <InfoCard
-              icon={LineChart}
-              label="Active Courses"
-              numberOfItems={2} />
-            <InfoCard
-              icon={LayoutList}
-              label="Finished Courses"
-              numberOfItems={1}
-              variant="success" />
-            <div className="border drop-shadow-sm filter rounded-custom flex items-center gap-5 p-5">
-              <IconBadge
-                icon={CalendarDays} />
-              <div>
-                <p className="font-medium">
-                  Calendar
-                </p>
-                <p className="text-gray-500 text-sm">
-                  March 18, 2024
-                </p>
-              </div>
+            <div className="grid grid-cols-2 sm:grid-cols-4 gap-4">
+          <InfoCard
+            icon={LineChart}
+            label="Active Courses"
+            numberOfItems={coursesInProgress.length}
+          />
+          <InfoCard
+            icon={LayoutList}
+            label="Finished Courses"
+            numberOfItems={completedCourses.length}
+            variant="success"
+          />
+          <div className="border drop-shadow-sm filter rounded-custom flex items-center gap-5 p-5">
+            <IconBadge
+              icon={CalendarDays}
+            />
+            <div>
+              <p className="font-medium">
+              Calendar
+              </p>
+              <p className="text-gray-500 text-sm">
+              March 27, 2024
+              </p>
             </div>
-            <div className="bg-black flex justify-between items-center hover:bg-gray-800 text-white font-bold py-2 px-5 rounded-custom shadow-sm">
-              <div>
-                You have new messages!
-              </div>
-              <div>
-                <MoveRight size={20} className="mx-2" />
-              </div>
+          </div>
+          <div className="bg-black flex justify-between items-center hover:bg-gray-800 text-white font-bold py-2 px-4 rounded-custom shadow-sm">
+            <div>
+              You have new messages!
+            </div>
+            <div>
+              <MoveRight size={20} className="mx-2"/>
             </div>
           </div>
         </div>
-      <div className="p-6">
-        <a href="">
-          <Image src="/img3.jpeg" alt="image" width={700} height={700} />
-        </a>
-      </div>
-    </>
+        <CoursesList
+            items={[...coursesInProgress, ...completedCourses]}
+          />
+    </div>
   )
 }
